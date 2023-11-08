b88f84038108534d38fe0b0bfbc079cac4a4a606
ashik pa <ashikpa003@gmail.com>
2023-11-08 12:14:22 +0400

    homepage setup

--- src/Redux/Reducers/productReducer.js
+++ src/Redux/Reducers/productReducer.js
@@ -1,1 +1,17 @@
-export const 
+import { SET_PRODUCTS } from "../Constants/productType";
+
+const initialState = {
+    products:[]
+}
+export const  productReducer =(state=initialState,action)=>{
+    switch (action.type) {
+        case SET_PRODUCTS:
+            return {...state,
+                products:action.payload
+
+            }
+                
+        default:
+            return state;
+    }
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,46 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../Redux/Actions/productAction";
+import { Col, Row } from "react-bootstrap";
 
 function Home() {
-  const fetchProducts = async()=>{
-    let {data} = await axios.get('https://fakestoreapi.com/products').catch((err)=>console.log(err))
-    console.log(data);
-
-  }
-  useEffect(()=>{
-    fetchProducts()
-  })
+  const products = useSelector((state) => state.allProducts.products);
+  console.log(products);
+  const dispatch = useDispatch();
+  const fetchProducts = async () => {
+    let { data } = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => console.log(err));
+    dispatch(setProducts(data));
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <Row className="mt-4 ms-4 mb-4">
+      {products?.map((product) => {
+        const { id, image, title, description } = product;
+        return (
+              <Col sm={12} md={8} lg={6} xl={3} key={id}>
+                <div className="card mb-3" style={{width: "18rem"}}>
+                  <img classNameName="card-img-top" style={{width:'280px',height:"320px"}} src={image} alt="Card image cap" />
+                  <div classNameName="card-body">
+                    <h5 classNameName="card-title">{title.slice(0,100)}</h5>
+                    <p classNameName="card-text">
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                    </p>
+                    <button  className="btn btn-primary">
+                      Cart
+                    </button>
+                  </div>
+                </div>
+              </Col>
+        );
+      })}
+    </Row>
+  );
 }
 
-export default Home
+export default Home;
